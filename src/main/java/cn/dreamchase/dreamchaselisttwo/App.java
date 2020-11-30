a650a705b976c1262c7e5f1e3ee4131a325a5aa4
15083267362@163.com <15083267362@163.com>
2020-12-01 00:23:10 +0800

    2020-12-01

--- src/main/java/cn/dreamchase/dreamchaselisttwo/App.java
+++ src/main/java/cn/dreamchase/dreamchaselisttwo/App.java
@@ -4,8 +4,7 @@ package cn.dreamchase.dreamchaselisttwo;
  * Hello world!
  *
  */
-public class App 
-{
+public class App {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
--- src/main/java/cn/dreamchase/dreamchaselisttwo/statements/PersonMapper.java
+++ src/main/java/cn/dreamchase/dreamchaselisttwo/statements/PersonMapper.java
@@ -4,13 +4,15 @@ import java.util.List;
 import java.util.Map;
 
 import cn.dreamchase.dreamchaselisttwo.pojos.Person;
+
 /**
- * getMapper() 方法的使用：
- * 1. 创建Mapper接口。
- * 2. 创建Mapper接口同名的mapper.xml文件
- * 3. 在mapper.xml文件中的namespace属性中的值为 mapper接口的全限定性类名
- * 4. 实例化SqlSessionFactory
- * 5. 
+ * getMapper() 方法的使用： 
+ * 1. 创建Mapper接口。 
+ * 2. 创建Mapper接口同名的mapper.xml文件 
+ * 3. 在mapper.xml文件中的namespace属性中的值为 mapper接口的全限定性类名 
+ * 4. 实例化SqlSessionFactory 
+ * 5.
+ * 
  * @author MENGDEFANG
  *
  */
@@ -26,10 +28,20 @@ public interface PersonMapper {
 
 	/**
 	 * 插入数据至数据库中
+	 * 传入参数为对象
 	 * 
 	 * @param person
 	 */
 	void insert(Person person);
+
+	/**
+	 * 传入参数为非对象第一种
+	 * @param id
+	 * @param name
+	 * @param age
+	 */
+	void insert(int id, String name, int age);
+	
 
 	/**
 	 * 更新数据库中的数据
